9bf34acdd3dc2e1083a2ed8d718aa643f4ae3601
redhotk1m <kim@adsl.no>
2019-03-26 14:05:30 +0100

    Startup

--- src/main/java/org/openjfx/MainApp.java
+++ src/main/java/org/openjfx/MainApp.java
@@ -16,7 +16,7 @@ public class MainApp extends Application {
 
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
-
+        System.out.println("test");
         stage.setTitle("JavaFX and Maven");
         stage.setScene(scene);
         stage.show();
